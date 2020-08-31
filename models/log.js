287ee85cd296d5662e2ef1c3dc1a2dedde041843
Cris Matson <crisjmatson@gmail.com>
2020-08-30 20:07:38 -0400

    working

--- models/log.js
+++ models/log.js
@@ -7,12 +7,13 @@ module.exports = (sequelize, DataTypes) => {
 		definition: {
 			type: DataTypes.STRING,
 			allowNull: false,
+			unique: true,
 		},
 		result: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-		ownerid: {
+		owner_id: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
 		},
